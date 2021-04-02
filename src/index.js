775d5f18807d25c65bd9c5f2c5cf594d21689495
GowPlay <vicentemirandaseijas@gmail.com>
2021-04-02 16:51:16 -0500

    Este es el commit par GitHub

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('Hola..!! vicente miranda seijas..')
+
+
+console.log('Hola..!! vicente miranda seijas..')
+console.log('Hola..!! vicente miranda seijas..')
+console.log('Hola..!! vicente miranda seijas..')
+
+const data = "HOLA VICENTE ENRIQUE"
+
+console.log(data);
